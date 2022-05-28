18ba663f82ada91fb9df8ad785d7a18fbb01cbe6
student23 <student23@pcoip.ki.agh.edu.pl>
2022-05-28 13:58:08 +0200

    add red color when email is to long

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <h1>Wpisałeś {{email}}</h1>
+    <h1 >Wpisałeś {{email}}</h1>
     <input type="text" v-model="email" >
-
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else id=red >Twój adres e-mail jest stanowczo za długi.</div>
     
   </div>
 </template>
@@ -23,4 +25,7 @@ data() {
 
 <style>
 
+#red {
+  color: red;
+}
 </style>
