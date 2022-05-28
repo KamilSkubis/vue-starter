abb8134c8c830fd0a3d1be784839d261a9cc16cd
student23 <student23@pcoip.ki.agh.edu.pl>
2022-05-28 15:18:58 +0200

    two components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="app">
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
-    <div v-if="!loggedIn">
-      <label id="switch"
-        >Zaloguj się e-mailem
-        <input type="text" v-model="email" />
-        <button @click="logMeIn()">Wchodzę</button></label
-      >
+    <div v-if="!email">
+      <login-form @login="logMeIn($event)"></login-form>
     </div>
     <div v-else>
       <h2>Witaj {{ email }}</h2>
@@ -16,20 +12,25 @@
 </template>
 
 <script>
+import "milligram";
+import LoginForm from "./LoginForm.vue";
+
 export default {
+  components: { LoginForm },
   data() {
     return {
       email: "",
-      loggedIn: false,
     };
   },
   methods: {
-    logMeIn() {
-      this.loggedIn = true;
+    logMeIn(username) {
+      this.email = username;
     },
     logOut() {
-      this.loggedIn = false;
       this.email = "";
+    },
+    logMeIn(username) {
+      this.email = username;
     },
   },
 };
