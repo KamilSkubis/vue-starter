0564e8a88dcc392600beca29f4d07ffe8addb5ba
student23 <student23@pcoip.ki.agh.edu.pl>
2022-05-28 15:33:40 +0200

    three login forms

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
     <div v-if="!email">
-      <login-form @login="logMeIn($event)"></login-form>
+      <login-form @login="logMeIn($event)" button-label="Wejdź"></login-form>
+      <login-form @login="logMeIn($event)" button-label="Wleć"></login-form>
+      <login-form
+        @login="logMeIn($event)"
+        :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"
+      ></login-form>
     </div>
     <div v-else>
       <h2>Witaj {{ email }}</h2>
