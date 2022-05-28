8b374afd827c726656b921e005c90656cd9cac4a
student23 <student23@pcoip.ki.agh.edu.pl>
2022-05-28 14:10:05 +0200

    press enter, will start alert

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
     <h1 >Wpisałeś {{email}}</h1>
-    <input type="text" v-model="email" >
+    <input type="text" v-model="email"  @keydown.enter=alertMyEmail()>
+    <button @click=alertMyEmail() >send</button>
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
     <div v-else id=red >Twój adres e-mail jest stanowczo za długi.</div>
-    
   </div>
 </template>
 
 <script>
 export default{
-
-
-
 data() {
   return {
     email: ''
   };
 }
+,methods: {
+  alertMyEmail() {
+    alert(this.email);
+  }
+}
+
 } 
 
 </script>
